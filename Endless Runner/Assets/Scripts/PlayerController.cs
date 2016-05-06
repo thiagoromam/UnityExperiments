6ff1bc19b199f9eaad6b8fa2d578da614b3cde99
Thiago Luis Romam Matheus <thiago.romam@outlook.com>
2016-05-06 15:00:48 -0300

    endless runner tutorial - until animation

---
+++ Endless Runner/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+    private Collider2D _collider;
+	private Animator _animator;
+
+    public float MoveSpeed;
+    public float JumpForce;
+    public bool Grounded;
+    public LayerMask WhatIsGround;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _collider = GetComponent<Collider2D>();
+		_animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Grounded = Physics2D.IsTouchingLayers(_collider, WhatIsGround);
+
+        _rigidbody.velocity = new Vector2(MoveSpeed, _rigidbody.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            if (Grounded)
+            {
+                _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, JumpForce);
+            }
+        }
+		
+		_animator.SetFloat("Speed", _rigidbody.velocity.x);
+		_animator.SetBool("Grounded", Grounded);
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+Unity Experiments
+===============
+
+Tests and experiments with Unity.
+
+Tutorials:
+* [Endless Runner](https://www.youtube.com/watch?v=GrQalFLtQT4&index=1&list=PLiyfvmtjWC_XmdYfXm2i1AQ3lKrEPgc9-)
