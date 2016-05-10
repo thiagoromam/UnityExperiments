480abb34964cb390d76a3572185388cdea367f9e
Thiago Luis Romam Matheus <thiago.romam@outlook.com>
2016-05-09 21:47:21 -0300

    changes until random plataforms

--- Endless Runner/Assets/Scripts/PlatformGenerator.cs
+++ Endless Runner/Assets/Scripts/PlatformGenerator.cs
@@ -2,31 +2,39 @@
 
 public class PlatformGenerator : MonoBehaviour
 {
-    private float _platformWidth;
-
-    public GameObject Platform;
+    private int _platformSelector;
+    private float[] _platformWidths;
+    
     public Transform GenerationPoint;
     public float DistanceBetween;
     public float DistanceBetweenMin;
     public float DistanceBetweenMax;
-    public ObjectPooler ObjectPool;
+    public ObjectPooler[] ObjectPools;
 
     void Start()
     {
-        _platformWidth = Platform.GetComponent<BoxCollider2D>().size.x;
+        _platformWidths = new float[ObjectPools.Length];
+        for (var i = 0; i < _platformWidths.Length; i++)
+        {
+            _platformWidths[i] = ObjectPools[i].PooledObject.GetComponent<BoxCollider2D>().size.x;
+        }
     }
 
     void Update()
     {
         if (transform.position.x < GenerationPoint.position.x)
         {
-            DistanceBetween = Random.Range(DistanceBetweenMin, DistanceBetweenMax);
-            transform.position += new Vector3(_platformWidth + DistanceBetween, 0);
+            _platformSelector = Random.Range(0, ObjectPools.Length);
 
-            var newPlatform = ObjectPool.GetPooledObject();
+            DistanceBetween = Random.Range(DistanceBetweenMin, DistanceBetweenMax);
+            transform.position += new Vector3(_platformWidths[_platformSelector] / 2 + DistanceBetween, 0);
+
+            var newPlatform = ObjectPools[_platformSelector].GetPooledObject();
             newPlatform.transform.position = transform.position;
             newPlatform.transform.rotation = transform.rotation;
             newPlatform.SetActive(true);
+            
+            transform.position += new Vector3(_platformWidths[_platformSelector] / 2, 0);
         }
     }
 }
