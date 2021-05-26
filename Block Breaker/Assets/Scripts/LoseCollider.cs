6fa11c93d192187a1fe4edf941658ecf61475fcd
Thiago Luis Romam Matheus <thiago.romam@outlook.com>
2021-05-25 21:12:58 -0300

    colliders, collision & triggers

---
+++ Block Breaker/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseCollider : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        SceneManager.LoadScene("Game Over");
+    }
+}
