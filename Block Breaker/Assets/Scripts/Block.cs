33d7ff401ef5527f3f99a511b809cde1343d758d
Thiago Luis Romam Matheus <thiago.romam@outlook.com>
2021-05-28 12:48:00 -0300

    play clip at point

--- Block Breaker/Assets/Scripts/Block.cs
+++ Block Breaker/Assets/Scripts/Block.cs
@@ -2,8 +2,12 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] private AudioClip _breakSound;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        AudioSource.PlayClipAtPoint(_breakSound, Camera.main.transform.position);
+
         Destroy(gameObject);
     }
 }
