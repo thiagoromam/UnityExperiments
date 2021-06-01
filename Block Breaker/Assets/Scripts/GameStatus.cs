a84a817e398a66ed260f429215f751bcd175bbc3
Thiago Luis Romam Matheus <thiago.romam@outlook.com>
2021-06-01 11:18:50 -0300

    single pattern bug fix

--- Block Breaker/Assets/Scripts/GameStatus.cs
+++ Block Breaker/Assets/Scripts/GameStatus.cs
@@ -14,6 +14,7 @@ public class GameStatus : MonoBehaviour
 
         if (gamesStatusCount > 1)
         {
+            gameObject.SetActive(false);
             Destroy(gameObject);
         }
         else
