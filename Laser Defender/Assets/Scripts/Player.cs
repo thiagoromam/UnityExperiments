fd6e7b6939938dbc881b2ed0b26448b6c16b8ba4
Thiago Luis Romam Matheus <thiago.romam@outlook.com>
2021-06-08 22:18:52 -0300

    viewPortToWorldPoint()

--- Laser Defender/Assets/Scripts/Player.cs
+++ Laser Defender/Assets/Scripts/Player.cs
@@ -3,12 +3,27 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float _moveSpeed = 10;
+    [SerializeField] float _padding = 1;
 
-    private void Update()
+    Vector3 _min;
+    Vector3 _max;
+
+    void Start()
+    {
+        SetUpMovementBoundaries();
+    }
+    void Update()
     {
         Move();
     }
 
+    private void SetUpMovementBoundaries()
+    {
+        Vector3 padding = new Vector3(_padding, _padding);
+
+        _min = Camera.main.ViewportToWorldPoint(Vector3.zero) + padding;
+        _max = Camera.main.ViewportToWorldPoint(Vector3.one) - padding;
+    }
     private void Move()
     {
         float inputY = Input.GetAxis("Vertical");
@@ -17,6 +32,9 @@ public class Player : MonoBehaviour
 
         position.x += inputX * _moveSpeed * Time.deltaTime;
         position.y += inputY * _moveSpeed * Time.deltaTime;
+
+        position.x = Mathf.Clamp(position.x, _min.x, _max.x);
+        position.y = Mathf.Clamp(position.y, _min.y, _max.y);
 
         transform.position = position;
     }
