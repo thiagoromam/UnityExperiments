0be7de57cd2f7fd748c5f857675c3e422398af64
Thiago Luis Romam Matheus <thiago.romam@outlook.com>
2021-06-09 22:51:49 -0300

    move enemy on path

--- Laser Defender/Assets/Scripts/EnemyPathing.cs
+++ Laser Defender/Assets/Scripts/EnemyPathing.cs
@@ -3,4 +3,37 @@ using UnityEngine;
 public class EnemyPathing : MonoBehaviour
 {
     [SerializeField] Transform[] _waypoints;
+    [SerializeField] float _moveSpeed = 2;
+
+    int _waypointIndex;
+
+    void Start()
+    {
+        transform.position = (Vector2)_waypoints[_waypointIndex].transform.position;
+    }
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (_waypointIndex < _waypoints.Length)
+        {
+            Vector2 targetPosition = _waypoints[_waypointIndex].transform.position;
+            float movementInThisFrame = _moveSpeed * Time.deltaTime;
+            Vector2 newPosition = Vector2.MoveTowards(transform.position, targetPosition, movementInThisFrame);
+
+            transform.position = newPosition;
+
+            if (newPosition == targetPosition)
+            {
+                _waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 }
