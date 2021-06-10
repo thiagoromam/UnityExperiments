e65fba7bf75f346c73b01be285a32e47f0b6fa69
Thiago Luis Romam Matheus <thiago.romam@outlook.com>
2021-06-09 23:01:13 -0300

    waveConfig scriptable object

---
+++ Laser Defender/Assets/Scripts/WaveConfig.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy Wave Config")]
+public class WaveConfig : ScriptableObject
+{
+    [SerializeField] GameObject _enemyPrefab;
+    [SerializeField] GameObject _pathPrefab;
+    [SerializeField] float _timeBetweenSpawns = 0.5f;
+    [SerializeField] float _spawnRandomFactor = 0.3f;
+    [SerializeField] int _numberOfNames = 5;
+    [SerializeField] float _moveSpeed = 2;
+
+    public GameObject EnemyPrefab => _enemyPrefab;
+    public GameObject PathPrefab => _pathPrefab;
+    public float TimeBetweenSpawns => _timeBetweenSpawns;
+    public float SpawnRandomFactor => _spawnRandomFactor;
+    public int NumberOfNames => _numberOfNames;
+    public float MoveSpeed => _moveSpeed;
+
+}
