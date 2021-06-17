fdf8ded34211c831190047ae91bcfdce354de17c
Thiago Luis Romam Matheus <thiago.romam@outlook.com>
2021-06-16 21:32:54 -0300

    music player with singleton

---
+++ Laser Defender/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    void Awake()
+    {
+        SetUpSingleton();
+    }
+
+    private void SetUpSingleton()
+    {
+        if (FindObjectsOfType<MusicPlayer>().Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
