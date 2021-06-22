14345449be831e2bd9c203521c23670210807789
Thiago Luis Romam Matheus <thiago.romam@outlook.com>
2021-06-21 22:23:41 -0300

    adding animation events

--- Glitch Garden/Assets/Scripts/Attacker.cs
+++ Glitch Garden/Assets/Scripts/Attacker.cs
@@ -3,9 +3,16 @@ using UnityEngine;
 public class Attacker : MonoBehaviour
 {
     [SerializeField, Range(0, 5)] float _walkSpeed = 1;
+    [SerializeField] bool _canWalk = false;
 
     void Update()
     {
-        transform.Translate(Vector2.left * _walkSpeed * Time.deltaTime);
+        if (_canWalk)
+            transform.Translate(Vector2.left * _walkSpeed * Time.deltaTime);
+    }
+
+    void OnWalkEnter()
+    {
+        _canWalk = true;
     }
 }
