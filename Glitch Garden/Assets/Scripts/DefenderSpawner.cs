d84f990e1ac7dcc9922c2dd506526d388a8690a9
Thiago Luis Romam Matheus <thiago.romam@outlook.com>
2021-06-22 22:04:34 -0300

    snap defenders to grid

--- Glitch Garden/Assets/Scripts/DefenderSpawner.cs
+++ Glitch Garden/Assets/Scripts/DefenderSpawner.cs
@@ -9,8 +9,19 @@ public class DefenderSpawner : MonoBehaviour
         GameObject defender = Instantiate(_defender, GetSquareClicked(), Quaternion.identity);
     }
 
+    private Vector2 SnapToGrid(Vector2 position)
+    {
+        position.x = Mathf.RoundToInt(position.x);
+        position.y = Mathf.RoundToInt(position.y);
+
+        return position;
+    }
+
     private Vector2 GetSquareClicked()
     {
-        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 grid = SnapToGrid(position);
+
+        return grid;
     }
 }
