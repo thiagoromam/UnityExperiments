5152ac8e149b026b61943f7f15e17407cf999739
Thiago Luis Romam Matheus <thiago.romam@outlook.com>
2021-06-23 22:11:32 -0300

    attacker in my lane

--- Glitch Garden/Assets/Scripts/Shooter.cs
+++ Glitch Garden/Assets/Scripts/Shooter.cs
@@ -5,8 +5,46 @@ public class Shooter : MonoBehaviour
     [SerializeField] GameObject _projectile;
     [SerializeField] Transform _gun;
 
+    AttackerSpawner _laneSpawner;
+
+    void Start()
+    {
+        SetLaneSpawner();
+    }
+    void Update()
+    {
+        if (IsAttackerInLane())
+        {
+            Debug.Log("shoot pew pew");
+        }
+        else
+        {
+            Debug.Log("sit and wait");
+        }
+    }
     void Fire()
     {
         Instantiate(_projectile, _gun.position, _gun.rotation);
+    }
+
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
+
+        foreach (var spawner in spawners)
+        {
+            bool isCloseEnough = Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon;
+
+            if (isCloseEnough)
+            {
+                _laneSpawner = spawner;
+
+                break;
+            }
+        }
+    }
+    private bool IsAttackerInLane()
+    {
+        return _laneSpawner.transform.childCount > 0;
     }
 }
