c82aab080e8e066ee0cb851d1f4d846553b3d603
Thiago Luis Romam Matheus <thiago.romam@outlook.com>
2021-06-23 21:17:56 -0300

    add resources as anim event

--- Glitch Garden/Assets/Scripts/Defender.cs
+++ Glitch Garden/Assets/Scripts/Defender.cs
@@ -3,4 +3,16 @@ using UnityEngine;
 public class Defender : MonoBehaviour
 {
     [SerializeField] int _starCost = 100;
+
+    private StarDisplay _starDisplay;
+
+    void Start()
+    {
+        _starDisplay = FindObjectOfType<StarDisplay>();
+    }
+
+    void AddStars(int amount)
+    {
+        _starDisplay.AddStars(amount);
+    }
 }
